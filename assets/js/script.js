6366bb13f1f05398ab71be8e95b4f731271cb8c8
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-07 13:51:54 +0200

    Exercice 4 corriger

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+$(function(){
+  //Je cible les enfants (.children()) de l'élément 'ol' et je modifie le css en indiquant dans les parenthèses la propriété 'color' et sa valeur 'red'
+    $('ol').children().css('color', 'red');
+});
